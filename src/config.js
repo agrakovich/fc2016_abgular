fe10db35b4a501232b0425fc1742c978ce429b93
a.grakovich <drem.aimlf@gmail.com>
2017-02-09 15:35:44 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    apiUrl: 'https://filmsdemo.herokuapp.com'
+    apiUrl: 'https://fcamp2016.herokuapp.com'
     //apiUrl: 'http://localhost:3000',
     authRoute: '/auth',
     articlesRoute: '/articles'
